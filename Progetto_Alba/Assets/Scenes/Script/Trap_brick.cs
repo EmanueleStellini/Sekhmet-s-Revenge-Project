7ceb95c644a0cc5fca5c07188b4f2d81487fc87d
EmanueleStellini <47296669+EmanueleStellini@users.noreply.github.com>
2019-02-05 21:44:31 +0100

    trap briks

---
+++ Progetto_Alba/Assets/Scenes/Script/Trap_brick.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_brick : MonoBehaviour {
+
+    private GameObject[] bricks;
+    private Rigidbody fisic;
+    private GameObject character;
+    private Collider characterCollider;
+
+    public string boh = "669";
+
+	// Use this for initialization
+	void Start () {
+
+        character = GameObject.Find("FPSController");
+        characterCollider = character.GetComponent<Collider>();
+
+        bricks = GameObject.FindGameObjectsWithTag("Brick");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == characterCollider)
+        {
+            for (int i=0; i<bricks.Length; i++)
+            {
+                fisic = bricks[i].GetComponent<Rigidbody>();
+                fisic.useGravity = true;
+            }
+        }
+        
+    }
+}
