721d81d47c54781e2e293c81a5d74796e5fd88e7
EmanueleStellini <47296669+EmanueleStellini@users.noreply.github.com>
2019-02-06 13:35:52 +0100

    trap1 with sound

--- Progetto_Alba/Assets/Scenes/Script/Trap_brick.cs
+++ Progetto_Alba/Assets/Scenes/Script/Trap_brick.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Trap_brick : MonoBehaviour {
 
     private GameObject[] bricks;
+    private GameObject brickSonor;
     private Rigidbody fisic;
     private GameObject character;
     private Collider characterCollider;
+    private AudioSource audioTrap;
+    public AudioClip clip;
+    private bool alreadyplayed = false;
+
 
     public string boh = "669";
 
@@ -18,6 +23,10 @@ public class Trap_brick : MonoBehaviour {
         characterCollider = character.GetComponent<Collider>();
 
         bricks = GameObject.FindGameObjectsWithTag("Brick");
+        brickSonor = GameObject.Find("BrickSonor");
+        audioTrap = brickSonor.GetComponent<AudioSource>();
+
+        
 
     }
 	
@@ -35,6 +44,12 @@ public class Trap_brick : MonoBehaviour {
                 fisic = bricks[i].GetComponent<Rigidbody>();
                 fisic.useGravity = true;
             }
+            if(alreadyplayed == false)
+            {
+                audioTrap.PlayOneShot(clip, 1);
+                alreadyplayed = true;
+            }         
+            
         }
         
     }
