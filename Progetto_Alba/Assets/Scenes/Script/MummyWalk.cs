bf394a3dccdac159b31783249d0b4615bf61b2c0
EmanueleStellini <47296669+EmanueleStellini@users.noreply.github.com>
2019-02-07 21:15:39 +0100

    Some Mummy Added

---
+++ Progetto_Alba/Assets/Scenes/Script/MummyWalk.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MummyWalk : MonoBehaviour {
+
+    private Animator MyAnimation;
+    private BoxCollider MyTrigCollider;
+    private GameObject MyCharacter;
+    private Collider CharacterCollider;
+
+	// Use this for initialization
+	void Start () {
+        MyAnimation = GetComponent<Animator>();
+        MyTrigCollider = GetComponent<BoxCollider>();
+
+        MyCharacter = GameObject.Find("FPSController");
+        CharacterCollider = MyCharacter.GetComponent<Collider>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == CharacterCollider)
+        {
+            MyAnimation.Play("Zombie Run");
+        }
+    }
+
+
+
+
+
+}
